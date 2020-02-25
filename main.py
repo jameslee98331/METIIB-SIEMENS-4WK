d66adaa2eee360107c1654bbd5a5d4e19ea60db7
jameslee98331 <jameslee.98331@gmail.com>
2020-02-25 17:44:43 +0000

    Add preproc functions

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-import orientation_detection
 import preproc
+import matplotlib.pyplot as plt
+
 
 def main():
-    pass
+    filename = 'input_files//img_preproc_1.jpg'
+    img = preproc.read_file(filename)
+
+    x_lim = (680, 2100)
+    y_lim = (460, 3400)
+    img_cropped = preproc.crop_img(img, y_lim, x_lim)
+
+    img_socket = img_cropped[1000:1412,1118:1271]
+    print(img_socket.shape)
+
+    plt.imshow(img_socket)
+    plt.show()
+
+# def img_masked(img,sample_range):
+#     img[sample_range]
+    for i in range(1000-10,1412):
+        for j in range(1118-20,1271):
+            img_cropped[i,j] = img_cropped[(1042,1166)]
+
+    plt.imshow(img_cropped)
+    plt.show()
+
+
 
 
 if __name__ == '__main__':
--- preproc.py
+++ preproc.py
@@ -1,5 +1,8 @@
 import cv2 as cv
-import numpy as np
 
 
+def read_file(file_name):
+    return cv.cvtColor(cv.imread(file_name), cv.COLOR_BGR2RGB)
 
+def crop_img(img, ylim, xlim):
+    return img[ylim[0]:ylim[1],xlim[0]:xlim[1]]
