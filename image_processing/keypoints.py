3466172be3d9d01870ee34263ee92791097c9542
jameslee98331 <jameslee.98331@gmail.com>
2020-02-26 15:49:14 +0000

    Add mask to keypoints.py

--- image_processing/keypoints.py
+++ image_processing/keypoints.py
@@ -3,31 +3,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-for i in range(1,4):
-    filename = f'input_files//preproc//img_preproc_{i}.jpg'
+n=2
+
+for i in range(1,n):
+    filename = f'..//files//input_files//preproc//img_preproc_{i}.jpg'
     img = cv.imread(filename)
 
     # Initiate ORB detector
     orb = cv.ORB_create()
 
+    # create a mask image filled with zeros, the size of original image
+    mask = np.zeros(img.shape[:2], dtype=np.uint8)
+
+    # draw your selected ROI on the mask image
+    mask1 = cv.rectangle(mask, (1180, 1040), (1220, 1080), (255), thickness=-1)
+    mask2 = cv.rectangle(mask, (1180, 1330), (1220, 1360), (255), thickness=-1)
+
     # find the keypoints with ORB
-    kp = orb.detect(img, None)
+    kp1 = orb.detect(img, mask1)
+    kp2 = orb.detect(img, mask2)
 
     # compute the descriptors with ORB
-    kp, des = orb.compute(img, kp)
-
-    # kp_0 = kp[0]
-    # # print(kp_0)
-    # # print(kp)
-    # # print(type(kp_0))
-    # # print(type(kp))
+    kp, des = orb.compute(img, kp1 + kp2)
 
     pts = cv.KeyPoint_convert(kp)
 
     # draw only keypoints location,not size and orientation
     img = cv.drawKeypoints(img, kp, img, color=(0,255,0), flags=0)
 
-    output_name = f'output_files//ORB_keypoints_{i}.jpg'
+    output_name = f'..//files//output_files//ORB_keypoints_{i}.jpg'
     cv.imwrite(output_name,img)
 
     plt.imshow(img)
