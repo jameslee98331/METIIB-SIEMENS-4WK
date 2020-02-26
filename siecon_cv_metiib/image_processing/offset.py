462cb1bde068ffcd20ba028c9220419dfd8a6714
jameslee98331 <jameslee.98331@gmail.com>
2020-02-26 22:04:29 +0000

    Add test_centroid

--- siecon_cv_metiib/image_processing/offset.py
+++ siecon_cv_metiib/image_processing/offset.py
@@ -4,7 +4,7 @@ import numpy as np
 def centroid(kp_coord: list):
 
     x = [kp_coord[i][0] for i in range(len(kp_coord))]
-    y = [kp_coord[i][0] for i in range(len(kp_coord))]
+    y = [kp_coord[i][1] for i in range(len(kp_coord))]
     return (np.mean(x), np.mean(y))
 
 def pixel_to_mm():
--- tests/image_processing/test_offset.py
+++ tests/image_processing/test_offset.py
@@ -3,4 +3,6 @@ from siecon_cv_metiib.image_processing import offset
 class TestOffset:
 
     def test_centroid(self):
-        pass
+        test_kp_coord = [(1,2), (2,3), (4,5), (3,2)]
+
+        assert offset.centroid(test_kp_coord) == (2.5, 3)
