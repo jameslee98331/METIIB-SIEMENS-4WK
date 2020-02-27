85b0621346325792b2b9d3ffebde46e855f28df2
jameslee98331 <jameslee.98331@gmail.com>
2020-02-27 23:50:26 +0000

    Add docstring to features.py

--- siecon_cv_metiib/image_processing/features.py
+++ siecon_cv_metiib/image_processing/features.py
@@ -1,20 +1,38 @@
 import cv2 as cv
+import numpy as np
 
 
-class KeyPointFeature:
+def keypts(img: np.ndarray, mask: np.ndarray) -> tuple:
+    """
+    Args:
+        img (np.ndarray): array of img for key point detection
+        mask (np.ndarray): array of mask for ROI
 
-    def __init__(self, img, mask):
-        self.img = img
+    Returns:
+        tuple:
+            - list of keypoint locations in tuples of (row[i], column[i])
+            - list of keypoint objects
+    """
 
-        # Initiate ORB detector
-        orb = cv.ORB_create()
+    # Initiate ORB detector
+    orb = cv.ORB_create()
 
-        # find the keypoints with ORB
-        self.keypts = orb.detect(img, mask)
+    # find the keypoints with ORB
+    keypts = orb.detect(img, mask)
 
-        # convert keypoint object to locations array
-        self.keypts_coord = cv.KeyPoint_convert(self.keypts)
+    # convert keypoint object to locations array
+    return cv.KeyPoint_convert(keypts), keypts
 
-    def keypts_img(self, img, key_points):
-        # draw only keypoints location,not size and orientation
-        return cv.drawKeypoints(img, key_points, img, color=(0, 255, 0), flags=0)
+
+def keypts_img(img: np.ndarray, key_points: cv.KeyPoint) -> np.ndarray:
+    """
+    Args:
+        img (np.ndarray): array of img for key point detection
+        key_points (cv.KeyPoint): list of keypoint objects
+
+    Returns:
+        img (np.ndarray): img input image overlaid with keypoints in green
+    """
+
+    # draw only keypoints location,not size and orientation
+    return cv.drawKeypoints(img, key_points, img, color=(0, 255, 0), flags=0)
