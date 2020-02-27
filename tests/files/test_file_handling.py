681e13f950bd5ca1fd2ef1f687f7b6f4b0894e5f
jameslee98331 <jameslee.98331@gmail.com>
2020-02-27 23:06:22 +0000

    Add placeholder code for testing file_handling fns

---
+++ tests/files/test_file_handling.py
@@ -0,0 +1,13 @@
+from siecon_cv_metiib.files import file_handling
+
+
+class TestFileHandling:
+
+    def test_dir_empty(tmpdir):
+        file_handling.dir_empty(tmpdir)
+        pass
+
+    def test_first_img(tmpdir):
+        file_handling.first_img(tmpdir)
+        pass
+
