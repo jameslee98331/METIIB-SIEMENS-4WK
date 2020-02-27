9678bd7bea0e3bbaf0fad83b0f7cd452a5bfd1c5
jameslee98331 <jameslee.98331@gmail.com>
2020-02-27 23:07:30 +0000

    Add preprocessing and feature detection logic to main loop

--- siecon_cv_metiib/main.py
+++ siecon_cv_metiib/main.py
@@ -1,10 +1,14 @@
-import cv2 as cv
+import numpy as np
 import matplotlib.pyplot as plt
-from siecon_cv_metiib.files import file_handling
-from siecon_cv_metiib.image_processing import features, offset, preproc
+from collections import namedtuple
+from files import file_handling
+from image_processing import offset, preproc
+from image_processing.features import KeyPointFeature
+from robot import robot_param
 
 
 DEBUG = True
+
 
 def main():
 
@@ -22,57 +26,134 @@ def main():
     #   - overwriting previous image
 
     # 3. File Handling
-    # The directory in which live images should be stored
-    img_dir = 'siecon_cv_metiib/files/live_img//'
+    # The directory in which live images is stored
+    img_dir = 'files/live_img//'
 
-    # continuously checks if there are images to be processed in the live_img/ directory
+    # Check if there are images to be processed in the live_img/ directory
     while file_handling.dir_empty(img_dir):
         continue
 
-    file_path = file_handling.full_path(img_dir)
-    img = cv.imread(file_path)
+    file_path = file_handling.first_img(img_dir)
 
     # 4. Pre-processing of Images
+    input_img = preproc.read_image(file_path)
+
+    # corner = (row, col)
+    # Defined limits to include only the G120C FSAA in the image frame
+    # THIS IS TO BE ADJUSTED WHEN THE VISION SYSTEM IS PUT INTO THE PRODUCTION ENVIRONMENT
+    # TODO: define these values in a config.py somewhere else so it is easier for deployment
+    FRAME_START = (460, 680)
+    FRAME_FINISH = (3400, 2100)
+
+    # TODO: define the namedtuple somewhere else
+    rect = namedtuple('rect', 'start, finish')
+    crop_rect = rect(FRAME_START, FRAME_FINISH)
+
+    cropped_img = preproc.crop(input_img, crop_rect)
+
     # TODO:
-    #   - preprocess image in live_img/ directory
-    #   - crop image to size
     #   - increase contrast
     #   - return processed image
-    # img = preproc.crop(img, start=(), finish=())
-
 
     # 5. Feature Detection
-    # draw your selected ROI on the mask image
-    mask0 = features.mask(img, start=(1180, 1040), finish=(1220, 1100))
-    mask1 = features.mask(img, start=(1180, 1300), finish=(1220, 1360))
+    rect = namedtuple('rect', 'start, finish')
 
-    kp0_coord = features.kp_coord(img, mask0)
-    kp1_coord = features.kp_coord(img, mask1)
+    # TODO: define these values in a config.py somewhere else so it is easier for deployment
+    MASK_0_START = (1180, 1040)
+    MASK_0_FINISH = (1220, 1100)
+    mask_0_rect = rect(MASK_0_START, MASK_0_FINISH)
+    mask_0 = preproc.mask(cropped_img, mask_0_rect)
+
+    feature = KeyPointFeature(cropped_img, mask_0)
+    key_pts_0_coord = feature.keypts_coord
+    key_pts_0 = feature.keypts
+
+    # TODO: define these values in a config.py somewhere else so it is easier for deployment
+    MASK_1_START = (1180, 1300)
+    MASK_1_FINISH = (1220, 1360)
+    mask_1_rect = rect(MASK_1_START, MASK_1_FINISH)
+    mask_1 = preproc.mask(cropped_img, mask_1_rect)
+
+    feature = KeyPointFeature(cropped_img, mask_1)
+    key_pts_1_coord = feature.keypts_coord
+    key_pts_1 = feature.keypts
 
     if DEBUG:
-        kp0 = features.kp(img, mask0)
-        kp1 = features.kp(img, mask1)
-        kp = kp0 + kp1
-        kp_img = features.kp_img(img, kp)
-        cv.imwrite('siecon_cv_metiib/files/output_files/ORB_keypoints.jpg', kp_img)
-        plt.imshow(kp_img)
+        key_pts_img = feature.keypts_img(cropped_img, key_pts_0 + key_pts_1)
+        plt.imshow(key_pts_img)
         plt.show()
 
-
     # 6. Offset Calculation
-    centroid_0 = offset.centroid(kp0_coord)
-    centroid_1 = offset.centroid(kp1_coord)
+    key_pts_0_centroid = offset.centroid(key_pts_0_coord)
+    key_pts_1_centroid = offset.centroid(key_pts_1_coord)
 
     if DEBUG:
-        print(centroid_0)
-        print(centroid_1)
+        print(key_pts_0_centroid)
+        print(key_pts_1_centroid)
 
     # TODO:
     #   - translate pixel offset to mm offset
     #   - return offset from reference datum as a float
 
+    X_RANGE_LEFT = (400, 430)
+    X_RANGE_RIGHT = (1010, 1040)
+    Y_RANGE_TOP = (350, 380)
+    Y_RANGE_BOT = (610, 640)
+
+    MASK_TOP_LEFT_START = (X_RANGE_LEFT[0], Y_RANGE_TOP[0])
+    MASK_TOP_LEFT_FINISH = (X_RANGE_LEFT[1], Y_RANGE_TOP[1])
+    mask_top_left_rect = rect(MASK_TOP_LEFT_START, MASK_TOP_LEFT_FINISH)
+    mask_top_left = preproc.mask(cropped_img, mask_top_left_rect)
+    feature = KeyPointFeature(cropped_img, mask_top_left)
+    key_pts_top_left_coord = feature.keypts_coord
+    key_pts_top_left_centroid = offset.centroid(key_pts_top_left_coord)
+    key_pts_top_left = feature.keypts
+
+    MASK_TOP_RIGHT_START = (X_RANGE_RIGHT[0], Y_RANGE_TOP[0])
+    MASK_TOP_RIGHT_END = (X_RANGE_RIGHT[1], Y_RANGE_TOP[1])
+    mask_top_right_rect = rect(MASK_TOP_RIGHT_START, MASK_TOP_RIGHT_END)
+    mask_top_right = preproc.mask(cropped_img, mask_top_right_rect)
+    feature = KeyPointFeature(cropped_img, mask_top_right)
+    key_pts_top_right_coord = feature.keypts_coord
+    key_pts_top_right_centroid = offset.centroid(key_pts_top_right_coord)
+    key_pts_top_right = feature.keypts
+
+    MASK_BOT_LEFT_START = (X_RANGE_LEFT[0], Y_RANGE_BOT[0])
+    MASK_BOT_LEFT_END = (X_RANGE_LEFT[1], Y_RANGE_BOT[1])
+    mask_bot_left_rect = rect(MASK_BOT_LEFT_START, MASK_BOT_LEFT_END)
+    mask_bot_left = preproc.mask(cropped_img, mask_bot_left_rect)
+    feature = KeyPointFeature(cropped_img, mask_bot_left)
+    key_pts_bot_left_coord = feature.keypts_coord
+    key_pts_bot_left_centroid = offset.centroid(key_pts_bot_left_coord)
+    key_pts_bot_left = feature.keypts
+
+    MASK_BOT_RIGHT_START = (X_RANGE_RIGHT[0], Y_RANGE_BOT[0])
+    MASK_BOT_RIGHT_END = (X_RANGE_RIGHT[1], Y_RANGE_BOT[1])
+    mask_bot_right_rect = rect(MASK_BOT_RIGHT_START, MASK_BOT_RIGHT_END)
+    mask_bot_right = preproc.mask(cropped_img, mask_bot_right_rect)
+    feature = KeyPointFeature(cropped_img, mask_bot_right)
+    key_pts_bot_right_coord = feature.keypts_coord
+    key_pts_bot_right_centroid = offset.centroid(key_pts_bot_right_coord)
+    key_pts_bot_right = feature.keypts
+
+    top_x = np.mean([(key_pts_top_right_centroid[0] - key_pts_top_left_centroid[0]),
+                    (key_pts_bot_right_centroid[0] - key_pts_bot_left_centroid[0])])
+    vert_y = np.mean([(key_pts_bot_left_centroid[1] - key_pts_top_left_centroid[1]),
+                     (key_pts_bot_right_centroid[1] - key_pts_top_right_centroid[1])])
+
+    pix_per_mm_0 = top_x / 33
+    pix_per_mm_1 = vert_y / 14
+    a = np.mean([pix_per_mm_0, pix_per_mm_1])
+
+    if DEBUG:
+        input_img = preproc.read_image(file_path)
+        cropped_img = preproc.crop(input_img, crop_rect)
+        key_pts_img = feature.keypts_img(cropped_img, key_pts_top_left + key_pts_top_right + key_pts_bot_left + key_pts_bot_right)
+        plt.imshow(key_pts_img)
+        plt.show()
 
     # 7. Robot control
+    robot_param.send()
     # TODO:
     #   - translate pixel offset to mm offset
     #   - return offset from reference datum as a float
