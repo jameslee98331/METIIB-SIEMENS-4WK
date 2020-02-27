a35b838e372cb7c98e4a006ea78a83ee6db45411
jameslee98331 <jameslee.98331@gmail.com>
2020-02-27 00:21:45 +0000

    Add y_centroid_rev function and test

--- siecon_cv_metiib/image_processing/offset.py
+++ siecon_cv_metiib/image_processing/offset.py
@@ -2,10 +2,12 @@ import numpy as np
 
 
 def centroid(kp_coord: list):
-
     x = [kp_coord[i][0] for i in range(len(kp_coord))]
     y = [kp_coord[i][1] for i in range(len(kp_coord))]
     return (np.mean(x), np.mean(y))
+
+def y_centroid_rev(img, centroid):
+    return (img.shape[1] - 1) - centroid[1]
 
 def pixel_to_mm():
     pass
