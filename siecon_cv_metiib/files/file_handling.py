23e5984c05b6a6dc2b9c75cda0aa7db9d3d9ab75
jameslee98331 <jameslee.98331@gmail.com>
2020-02-27 23:17:56 +0000

    Add docstring to functions

--- siecon_cv_metiib/files/file_handling.py
+++ siecon_cv_metiib/files/file_handling.py
@@ -2,6 +2,17 @@ import os
 
 
 def dir_empty(dirName):
+    """
+    Args:
+        dirName (str): file path to directory containing image
+
+    Returns:
+        bool: True for empty directory, False for img present
+
+    Raises:
+        Exception: if img directory does not exist
+    """
+
     if os.path.exists(dirName) and os.path.isdir(dirName):
         if os.listdir(dirName):
             return False
@@ -11,6 +22,14 @@ def dir_empty(dirName):
         raise Exception("Given Directory don't exists")
 
 def first_img(dirName):
+    """
+    Args:
+        dirName (str): file path to directory containing image
+
+    Returns:
+        str: file path to the first item in the directory
+    """
+
     filename = os.listdir(dirName)
 
     # Only reads the first file in the directory
