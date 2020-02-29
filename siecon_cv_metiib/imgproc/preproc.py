d99fe226c180f77eef9ae65e107750e7714b2069
jameslee98331 <jameslee.98331@gmail.com>
2020-02-29 16:45:31 +0000

    Rename image_processing/ to imgproc/

--- siecon_cv_metiib/imgproc/preproc.py
+++ siecon_cv_metiib/imgproc/preproc.py
@@ -3,18 +3,6 @@ import numpy as np
 from collections import namedtuple
 
 
-def read_image(filepath: str) -> np.ndarray:
-    """
-    Args:
-        filepath (str): file path to the image being read
-
-    Returns:
-        np.ndarray: array of the image with RGB values
-    """
-
-    return cv.cvtColor(cv.imread(filepath), cv.COLOR_BGR2RGB)
-
-
 def crop(img: np.ndarray, corner: namedtuple) -> np.ndarray:
     """
     Args:
