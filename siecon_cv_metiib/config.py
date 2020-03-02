a34d359ce772f623ca6dd7de6b02502356ac7af0
jameslee98331 <jameslee.98331@gmail.com>
2020-03-02 16:00:58 +0000

    Add configuration dicts

--- siecon_cv_metiib/config.py
+++ siecon_cv_metiib/config.py
@@ -1,3 +1,21 @@
-configuration = {
+dev_crop = {
+    'START': (460, 680),
+    'FINISH': (3400, 2100),
+}
 
+dev_mask_0 = {
+    'START': (1180, 1040),
+    'FINISH': (1220, 1100),
+}
+
+dev_mask_1 = {
+    'START': (1180, 1300),
+    'FINISH': (1220, 1360),
+}
+
+dev_calibration_rect = {
+    'X_RANGE_LEFT': (400, 430),
+    'X_RANGE_RIGHT': (1010, 1040),
+    'Y_RANGE_TOP': (350, 380),
+    'Y_RANGE_BOT': (610, 640),
 }
