629dad3a9c6ea425b82fb683dcb99539c969368a
jameslee98331 <jameslee.98331@gmail.com>
2020-03-05 00:18:53 +0000

    Add unit tests to imgproc fuctions

--- tests/test_imgproc.py
+++ tests/test_imgproc.py
@@ -5,12 +5,30 @@ from siecon_cv_metiib import imgproc
 
 class TestImgProc:
 
-    def test_centroid(self):
-        test_kp_coord = [(1, 2), (2, 3), (4, 5), (3, 2)]
+    def test_draw_rect(self):
+        test_start = (2, 2)
+        test_finish = (5, 5)
 
-        assert imgproc.centroid(test_kp_coord) == (2.5, 3)
+        test_rect = imgproc.draw_rect(test_start, test_finish)
 
-    def test_mask(self):
+        assert test_rect.start == (2, 2)
+        assert test_rect.finish == (5, 5)
+
+    def test_crop(self):
+        test_img = np.asarray([[(1,2,3),(2,3,4),(3,4,5),(4,5,6),(1,2,3)],
+                               [(2,3,4),(5,6,7),(1,2,3),(1,2,3),(4,5,6)],
+                               [(4,5,6),(7,8,9),(5,6,7),(7,6,5),(1,2,3)],
+                               [(3,4,5),(7,8,5),(1,2,4),(4,5,6),(5,6,7)],
+                               [(8,6,7),(9,0,0),(8,6,5),(1,3,4),(1,2,3)]])
+        test_start = (2, 2)
+        test_finish = (4, 4)
+        test_rect = namedtuple('rect', 'start, finish')
+        test_corner = test_rect(test_start, test_finish)
+
+        assert np.array_equal(imgproc.crop(test_img, test_corner), np.asarray([[(5,6,7),(7,6,5)],
+                                                                               [(1,2,4),(4,5,6)]]))
+
+    def test_draw_mask(self):
         test_img = np.asarray([[(1,2,3),(2,3,4),(3,4,5),(4,5,6),(1,2,3)],
                                [(2,3,4),(5,6,7),(1,2,3),(1,2,3),(4,5,6)],
                                [(4,5,6),(7,8,9),(5,6,7),(7,6,5),(1,2,3)],
@@ -21,7 +39,7 @@ class TestImgProc:
         rect = namedtuple('rect', 'start, finish')
         mask_rect = rect(test_start, test_finish)
 
-        test_mask = imgproc.mask(test_img, mask_rect)
+        test_mask = imgproc.draw_mask(test_img, mask_rect)
 
         assert np.array_equal(test_mask, [[0,0,0,0,0],
                                           [0,255,255,255,0],
@@ -29,14 +47,14 @@ class TestImgProc:
                                           [0,255,255,255,0],
                                           [0,0,0,0,0]])
 
+    def test_centroid(self):
+        test_kp_coord = [(1, 2), (2, 3), (4, 5), (3, 2)]
 
-    # TODO: Create new y_centroid_rev function in offset.py when location of reference is known
-    # def test_y_centroid_rev(self):
-    #     test_centroid = (4, 3)
-    #     test_img = np.asarray([[(1,2,3),(2,3,4),(3,4,5),(4,5,6),(1,2,3)],
-    #                            [(2,3,4),(5,6,7),(1,2,3),(1,2,3),(4,5,6)],
-    #                            [(4,5,6),(7,8,9),(5,6,7),(7,6,5),(1,2,3)],
-    #                            [(3,4,5),(7,8,5),(1,2,4),(4,5,6),(5,6,7)],
-    #                            [(8,6,7),(9,0,0),(8,6,5),(1,3,4),(1,2,3)]])
-    #
-    #     assert offset.y_centroid_rev(test_img, test_centroid) == 1
+        assert imgproc.centroid(test_kp_coord) == (2.5, 3)
+
+    def test_find_scale(self):
+        test_loc = [(5, 10), (10, 10), (5, 20), (10, 20)]
+        test_col_const = 2.5
+        test_row_const = 4
+
+        assert imgproc.find_scale(test_loc, test_col_const, test_row_const) == 2.25
